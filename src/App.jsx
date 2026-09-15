8b4d7da6e32cab72c352b05587e1d2959fefbbfd
Heshan200166 <202740692+Heshan200166@users.noreply.github.com>
2026-09-15 12:34:09 +0530

    chore: add project dependencies and media assets

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import WhatsAppFloat from './components/WhatsAppFloat';
+import Home from './pages/Home';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Gallery from './pages/Gallery';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
+
+function App() {
+  return (
+    <>
+      <ScrollToTop />
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/gallery" element={<Gallery />} />
+        </Routes>
+      </main>
+      <Footer />
+      <WhatsAppFloat />
+    </>
+  );
+}
+
+export default App;
